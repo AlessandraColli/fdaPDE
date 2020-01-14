1ebc165b1fea0504694041f790e170daf05cb619
eleonoraarnone <eleonora.arnone@polimi.it>
2020-01-14 16:27:12 +0100

    Come sopra

--- R/testFunctions.R
+++ R/testFunctions.R
@@ -37,26 +37,37 @@
 #' plot(FEMfunction)
 #' @export
 
-fs.test <- function (x, y, r0 = 0.1, r = 0.5, l = 3, b = 1) 
-{
+fs.test <- function(x,y,r0=.1,r=.5,l=3,b=1)
+  ## test function based on Tim Ramsay (2002) J.R.Statist. Soc. B
+  ## 64(2):307-319 "Spline smoothing over difficult regions"
+  { q <- pi*r/2 ## 1/2 length of semi-circle part of centre curve
+  a <- d <- x*0 ## along and distance to arrays
   
-  q <- pi * r/2
-  a <- d <- x * 0
+  ## convert x,y to along curve and distance to curve (a,d) 
+  ## co-ordinates. 0 distance along is at (x=-r,y=0)  
   
-  ind <- x >= 0 & y > 0
+  ind <- x>=0 & y>0
   a[ind] <- q + x[ind]
-  d[ind] <- y[ind] - r
+  d[ind] <- y[ind]-r
   
-  ind <- x >= 0 & y <= 0
-  a[ind] <- (-q - x[ind])
+  ind <- x>=0 & y<=0
+  a[ind] <- -q - x[ind]
   d[ind] <- -r - y[ind]
   
-  ind <- x < 0
-  a[ind] <- -atan(y[ind]/x[ind]) * r 
-  d[ind] <-( sqrt(x[ind]^2 + y[ind]^2) - r )* (y[ind]/r0*(as.numeric(abs(y[ind])<=r0 & x[ind]>-0.5))+(as.numeric(abs(y[ind])>r0 || x[ind]<(-0.5))))
   
-  f <- a * b + d^2
+  ind <- x < 0 
+  a[ind] <- -atan(y[ind]/x[ind])*r
+  d[ind] <- sqrt(x[ind]^2+y[ind]^2) - r
   
+  ## create exclusion index
+  
+  ind <- abs(d)>r-r0 | (x>l & (x-l)^2+d^2 > (r-r0)^2)
+  
+  # f <- a*b # the original
+  f <- a*b+d^2
+  
+  if (exclude) f[ind] <- NA
+  attr(f,"exclude") <- ind
   f
 }
 
